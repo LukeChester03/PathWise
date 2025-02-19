a61f8cf20ef96b5d611a45f1ae9379839b58b4a9
Luke Chester <lukechester03@outlook.com>
2025-02-19 15:29:37 +0000

    fixed _layout showing redundant screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { RootStackParamList } from "./navigation/types";
-import LoginScreen from "./screens/LoginScreen";
-import RegisterScreen from "./screens/RegisterScreen";
-import ForgotPasswordScreen from "./screens/ForgotPasswordScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import ExploreScreen from "./screens/ExploreScreen";
@@ -19,9 +16,6 @@ export default function Layout() {
       initialRouteName="Landing"
       screenOptions={{ animation: "fade", headerShown: false }}
     >
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Register" component={RegisterScreen} />
-      <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
       <Stack.Screen name="Explore" component={ExploreScreen} />
