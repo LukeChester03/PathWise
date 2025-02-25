6667f1cf23964b70c8352848f78304879320e805
Luke Chester <lukechester03@outlook.com>
2025-02-25 16:49:06 +0000

    redesigned details card

--- app/components/Map/DetailsCard.tsx
+++ app/components/Map/DetailsCard.tsx
@@ -1,4 +1,3 @@
-// src/components/DetailsCard.tsx
 import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 import { Colors } from "../../constants/colours";
@@ -12,10 +11,15 @@ interface DetailsCardProps {
 const DetailsCard: React.FC<DetailsCardProps> = ({ placeName, travelTime, distance }) => {
   return (
     <View style={styles.cardContainer}>
+      {/* GIF on the left */}
       <Image source={require("../../assets/walking.gif")} style={styles.gif} />
-      <Text style={styles.title}>Travelling to {placeName}</Text>
-      <Text style={styles.info}>Time left: {travelTime}</Text>
-      <Text style={styles.info}>Distance: {distance}</Text>
+
+      {/* Text in a column to the right of the GIF */}
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>Travelling to {placeName}</Text>
+        <Text style={styles.info}>Time left: {travelTime}</Text>
+        <Text style={styles.info}>Distance: {distance}</Text>
+      </View>
     </View>
   );
 };
@@ -23,38 +27,44 @@ const DetailsCard: React.FC<DetailsCardProps> = ({ placeName, travelTime, distan
 const styles = StyleSheet.create({
   cardContainer: {
     position: "absolute",
-    top: 20,
-    left: 20,
-    width: 200,
-    height: 240,
+    top: 20, // Position at the top of the screen
+    left: 20, // Position at the left of the screen
+    flexDirection: "row", // Align GIF and text horizontally
     backgroundColor: "white",
     borderRadius: 10,
-    padding: 20,
+    padding: 16,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
     },
+    width: 240,
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5,
+    elevation: 6,
+    alignItems: "center",
   },
   gif: {
     width: 50,
     height: 50,
-    alignSelf: "center",
-    marginBottom: 10,
+    marginRight: 16,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  textContainer: {
+    flex: 1, // Take up remaining space
+    justifyContent: "center", // Center text vertically
   },
   title: {
     fontSize: 16,
     fontWeight: "bold",
-    marginBottom: 10,
-    textAlign: "left",
+    marginBottom: 8, // Add spacing between title and info
+    color: Colors.primary, // Use a primary color for emphasis
   },
   info: {
-    fontSize: 16,
-    marginBottom: 10,
-    textAlign: "left",
+    fontSize: 14,
+    marginBottom: 6, // Add spacing between info lines
+    color: Colors.text, // Use a neutral color for info
   },
 });
 
