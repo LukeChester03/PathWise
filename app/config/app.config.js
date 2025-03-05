7fb1fdb2547aebac107a6b76f853349bec1217db
Luke Chester <lukechester03@outlook.com>
2025-03-05 23:54:42 +0000

    fixing build issues

--- app/config/app.config.js
+++ app/config/app.config.js
@@ -1,17 +1,3 @@
 module.exports = ({ config }) => {
-  const newConfig = {
-    ...config,
-    android: {
-      ...config.android,
-      // Add manifest attributes to handle conflicts
-      androidManifest: {
-        ...config.android?.androidManifest,
-        application: {
-          "@tools:replace": "android:appComponentFactory",
-        },
-      },
-    },
-  };
-
-  return newConfig;
+  return config;
 };
