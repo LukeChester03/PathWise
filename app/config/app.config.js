448016f92a7d0614b18500a3bf433f611d753d03
Luke Chester <lukechester03@outlook.com>
2025-03-14 17:26:12 +0000

    updated json

--- app/config/app.config.js
+++ app/config/app.config.js
@@ -1,16 +1,3 @@
 module.exports = ({ config }) => {
-  // Make sure the Google Maps API key is properly set
-  if (config.android && config.android.config && config.android.config.googleMapsApiKey) {
-    return config;
-  }
-  return {
-    ...config,
-    android: {
-      ...config.android,
-      config: {
-        ...config.android.config,
-        googleMapsApiKey: "AIzaSyDAGq_6eJGQpR3RcO0NrVOowel9-DxZkvA",
-      },
-    },
-  };
+  return config;
 };
